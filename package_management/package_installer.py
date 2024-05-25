954d02d6ded9ae8c7e550305000502e9fd9d5121
lukaszwelnic <lukaswelnic@hotmail.com>
2024-05-25 14:41:36 +0200

    ReadMe.md update

---
+++ package_management/package_installer.py
@@ -0,0 +1,7 @@
+class PackageInstaller:
+    def __init__(self, package_storage):
+        self.package_storage = package_storage
+
+    def install(self, package_name):
+        # Logic to install the given package
+        pass
