826e21bffa1ad5a820c17194d6137b89607d2e61
lukaszwelnic <lukaswelnic@hotmail.com>
2024-05-25 15:37:28 +0200

    package managemnt classes initialization

---
+++ package_management/package_updater.py
@@ -0,0 +1,7 @@
+class PackageUpdater:
+    def __init__(self, package_storage):
+        self.package_storage = package_storage
+
+    def update(self, package_name):
+        # Logic to install the given package
+        pass
