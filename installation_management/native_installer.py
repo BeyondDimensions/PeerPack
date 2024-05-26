b82f55d874540dac44f39fddd55ef42b80b4f80d
Na7h4na3l <124286699+Na7h4na3l@users.noreply.github.com>
2024-05-26 09:43:09 +0200

    uninstall and get installed added

--- installation_management/native_installer.py
+++ installation_management/native_installer.py
@@ -1,12 +1,41 @@
+import subprocess
+
 def install_package(download_path: str):
-        pulsar_package = subprocess.run('ppm install download_path', shell = True, capture_output = True, text = True)
-    # if(!success) raise Exception("Something went wrong with the installation.")
+    try:
+        result = subprocess.run('ppm install download_path', shell = True, capture_output = True, text = True)
+        output = result.stdout.strip()
+        error_out = result.stderr.strip()
+        return output, error_out, result.returncode
+
+        # if(!success) raise Exception("Something went wrong with the installation.")
+    except subprocess.CalledProcessError as e:
+        print(f"Error: {e}")
+        return None, str(e), e.returncode
     pass
 
 def uninstall_package(package_name: str):
+    try:
+        result = subprocess.run('ppm uninstall download_path', shell = True, capture_output = True, text = True)
+        output = result.stdout.strip()
+        error_out = result.stderr.strip()
+        return output, error_out, result.returncode
     # if(!success) raise Exception("Something went wrong with uninstallation.")
+    except subprocess.CalledProcessError as e:
+        print(f"Error: {e}")
+        return None, str(e), e.returncode
     pass
 
 def get_installed_packages():
-    # return [{"package_name": "example_name", "version": "0.0.1"}]
-    return []
+    try:
+        result = subprocess.run('ppm view package_name', shell = True, capture_output = True, text = True)
+        output = result.stdout.strip()
+        error_out = result.stderr.strip()
+        return output, error_out, result.returncode
+
+    except subprocess.CalledProcessError as e:
+        print(f"Error: {e}")
+        return None, str(e), e.returncode
+
+if __name__ == "__main__":
+    print(f"Output:\n{output}")
+    print(f"Error:\n{error_out}")
